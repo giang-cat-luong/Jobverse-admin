db2d88a778d4e41f26601c3553aec8062c5906cb
Jack Vu <giang.cat.luongg@gmail.com>
2025-05-15 20:56:39 +0700

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,56 @@
-// middleware.ts
 import { getToken } from "next-auth/jwt";
 import { NextResponse, type NextRequest } from "next/server";
 
-const secret = process.env.AUTH_SECRET;
+const secret = "keFM/VZT0Vs85k0f1kPm3xWZGJVW2sANB05l6QC4IOE="
+
+const roleBasedRoutes: Record<"employer" | "freelancer", string[]> = {
+  employer: ["/employer/jobs", "/employer/applicants"],
+  freelancer: ["/freelancer/jobs", "/freelancer/proposals"],
+};
+
+const protectedRoutes = Object.values(roleBasedRoutes).flat();
 
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
+  // Nếu route không được bảo vệ, cho phép truy cập
+  if (!protectedRoutes.some((route) => pathname.startsWith(route))) {
+    return NextResponse.next();
+  }
+
   if (!secret) {
-    console.error("NEXTAUTH_SECRET is missing!");
+    console.error("❌ AUTH_SECRET is missing!");
     throw new Error("Authentication secret is not configured");
   }
 
   const token = await getToken({ req: request, secret });
 
-  if (!token && pathname === "/") {
-    return NextResponse.redirect(new URL("/login?redirect=/", request.url));
+  if (!token) {
+    const callbackUrl = encodeURIComponent(request.nextUrl.pathname);
+    return NextResponse.redirect(
+      new URL(`/login?redirect=${callbackUrl}`, request.url)
+    );
   }
 
-  if (token && pathname === "/login") {
+  const userRoles = token.role as string[]; // mảng role
+
+  // Nếu user vào /seller nhưng không có role freelancer → redirect
+  if (pathname.startsWith("/seller") && !userRoles.includes("freelancer")) {
+    return NextResponse.redirect(new URL("/start-selling", request.url));
+  }
+
+  console.log("Token:", token);
+  console.log("Pathname:", pathname);
+  console.log("Role:", token?.role);
+  console.log("key:", secret);
+
+  // Kiểm tra xem user có quyền truy cập route hiện tại không
+  const isAuthorized = userRoles.some((role) => {
+    const allowedRoutes = roleBasedRoutes[role as "employer" | "freelancer"];
+    return allowedRoutes?.some((route) => pathname.startsWith(route));
+  });
+
+  if (!isAuthorized) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
@@ -26,5 +58,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/", "/login"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
